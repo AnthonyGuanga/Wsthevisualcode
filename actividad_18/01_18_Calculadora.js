46119e41cf591d7807d7aa64ad7af10bb591d558
AnthonyGu <ninfrad23@gmail.com>
2024-04-08 14:18:00 +0200

    add act18

---
+++ actividad_18/01_18_Calculadora.js
@@ -0,0 +1,9 @@
+
+function SUMAR(){
+    let a = document.getElementById("primerNumero");
+    let b = document.getElementById("segundoNumero")
+    let resu = document.getElementById("resultado")
+
+   let sum = a + b;
+   console.log(sum)
+}
