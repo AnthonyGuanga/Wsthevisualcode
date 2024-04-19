085e847094de9f132c0c6fe1788e733c035bf174
AnthonyGu <ninfrad23@gmail.com>
2024-04-19 12:25:19 +0200

    add act 19 sin completar

---
+++ actividad_19/activi19_01_js.js
@@ -0,0 +1,9 @@
+function contadorNumeros(){
+    let prNumero = document.prNumero
+    let seNumero = document.prNumero
+
+
+    for (let i = prNumero; i <= seNumero; i++) {
+    }
+    document.terNumero = result  
+}
